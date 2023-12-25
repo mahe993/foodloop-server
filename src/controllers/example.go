8f15058c2cb9a237b7883963ed638bedf12c8d12
Ma He <qmahe@live.com>
2023-12-25 20:35:06 +0800

    fix CI failing

--- src/controllers/example.go
+++ src/controllers/example.go
@@ -2,11 +2,3 @@
 Package controller breaks down requests by its method and further routes the requests to respective services.
 */
 package controllers
-
-func init() {
-	controller()
-}
-
-func controller () {
-	// test ci
-}
--- src/services/example.go
+++ src/services/example.go
@@ -2,11 +2,3 @@
 Package services consumes the requests and provides the business logic pertaining to the request.
 */
 package services
-
-func init() {
-	service()
-}
-
-func service () {
-	// test ci
-}
