d11ddaa6930c736f5bfed9104aafebbc819d1e5f
Ma He <qmahe@live.com>
2023-12-25 20:21:10 +0800

    test CI

--- src/controllers/example.go
+++ src/controllers/example.go
@@ -2,3 +2,11 @@
 Package controller breaks down requests by its method and further routes the requests to respective services.
 */
 package controllers
+
+func init() {
+	controller()
+}
+
+func controller () {
+	// test ci
+}
