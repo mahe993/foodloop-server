ceca85954d02acc748cb92a8c53098e208cc4d21
chae-seong <chaeseong123@hotmail.com>
2024-03-21 21:31:13 +0800

    seed data

---
+++ foodloop-db-files/seeders/03_insertFoodlist.sql
@@ -0,0 +1,7 @@
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('burger', '12:00:00', 'Monday', 1);
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('pizza', '12:30:00', 'Tuesday', 1);
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('drinks', '18:00:00', 'Wednesday', 1);
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('salad', '13:00:00', 'Monday', 1);
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('pastry', '12:30:00', 'Tuesday', 1);
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('rice', '20:00:00', 'Thursday', 1);
+INSERT INTO foodloop.foodlist (foodlistName, foodlistTime, foodlistDay, foodlistCurrIdx) VALUES ('exotic', '12:00:00', 'Friday', 1);
