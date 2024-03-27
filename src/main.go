6c04bdf6c85c9e7f84e9bf69bdbeda99f300ad2b
Ma He <qmahe@live.com>
2024-03-27 17:46:03 +0800

    fix deployment port

--- src/main.go
+++ src/main.go
@@ -13,10 +13,12 @@ Other modules:
 package main
 
 import (
+	"fmt"
 	"foodloop/src/database"
 	"foodloop/src/routers"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -61,7 +63,6 @@ func main() {
 	r.Mount(basePath+"/example", routers.Example.Router())
 	r.Mount(basePath+"/user", routers.User.Router())
 	r.Mount(basePath+"/foodlist", routers.Foodlist.Router())
-
-	log.Println("Starting server on port 1111")
-	log.Fatal(http.ListenAndServe(":1111", r))
+	log.Printf("Starting server on port %s", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r))
 }
