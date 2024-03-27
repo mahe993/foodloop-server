630c283dd7fea581596a9c2f5b8e804f7c673471
Ma He <qmahe@live.com>
2024-03-28 01:14:38 +0800

    fix seed

--- foodloop-db-files/seeders/04_insertPeopleToFoodlist.sql
+++ foodloop-db-files/seeders/04_insertPeopleToFoodlist.sql
@@ -1,5 +1,6 @@
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (1, 1);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (2, 2);
+INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (3, 3);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (4, 4);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (5, 5);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (6, 6);
@@ -11,4 +12,3 @@ INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (11, 11);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (12, 12);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (13, 13);
 INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (14, 14);
-INSERT INTO foodloop.peopleToFoodlist (peopleID, foodlistID) VALUES (15, 15);
